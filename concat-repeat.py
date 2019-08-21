85ffe0e1a3e1b3b534e50030075519efbd683ca6
fahad <fahad@datumcorp.com>
2019-08-21 09:52:30 +0800

    index-count

--- concat-repeat.py
+++ concat-repeat.py
@@ -1,6 +1,6 @@
 fahad= ["hello how are you"]
 faisal= ["whats wrong"]
-print(fahad + faisal)
+print(fahad + faisal) #concat
 
-print([0] * 4)
+print([0] * 4)  # repeat
 
---
+++ count - index.py
@@ -0,0 +1,3 @@
+fahad = ["what","yes", "so", "ok", "how", "for", "this", "that"]
+print(fahad.index("this"))
+print(fahad.count("yes"))
